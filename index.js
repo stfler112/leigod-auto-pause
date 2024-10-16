9a0b8b19b055d8b95dd77403886c38c701f770d8
luanche <1054129686@qq.com>
2024-10-16 11:23:03 +0800

    add action log & format code

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-import 'dotenv/config'
-import { LeigodAPI } from "./leigod.js";
-
+import 'dotenv/config';
+import { LeigodAPI } from './leigod.js';
+import * as core from '@actions/core';
 
 const api = new LeigodAPI();
 
-const usernames = process.env.USERNAME_ARR.split(',')
-const passwords = process.env.PASSWORD_ARR.split(',')
+const usernames = process.env.USERNAME_ARR.split(',');
+const passwords = process.env.PASSWORD_ARR.split(',');
 
 if (!usernames || !passwords) {
-    console.error("please set USERNAME_ARR and PASSWORD_ARR in envs (split multi user by ',')")
+  core.setFailed(
+    "please set USERNAME_ARR and PASSWORD_ARR in envs (split multi user by ',')",
+  );
 }
 
 const users = [];
 
 for (let idx = 0; idx < usernames.length; idx++) {
-    const username = (usernames[idx] || '').trim();
-    const password = (passwords[idx] || '').trim();
-    users.push({ username, password })
+  const username = (usernames[idx] || '').trim();
+  const password = (passwords[idx] || '').trim();
+  users.push({ username, password });
 }
 
 async function pause(username, password) {
-    if (username && password) {
-        try {
-            console.info("Login in to " + username);
-            await api.login(username, password);
-            let isPaused = await api.isTimePaused()
-            console.info("Getting pause status: " + isPaused);
-            if (!isPaused) {
-                console.info("Trying to pause time");
-                await api.pauseTime()
-                isPaused = await api.isTimePaused()
-                console.info("Getting pause status again: " + isPaused);
-            }
-        } catch (error) {
-            console.error(error)
-        }
-
-    } else {
-        console.error("username or password is empty, please check envs" + username)
+  if (username && password) {
+    try {
+      core.info('Login in to ' + username);
+      await api.login(username, password);
+      let isPaused = await api.isTimePaused();
+      core.info('Getting pause status: ' + isPaused);
+      if (!isPaused) {
+        core.warning('Trying to pause time');
+        await api.pauseTime();
+        isPaused = await api.isTimePaused();
+        core.info('Getting pause status again: ' + isPaused);
+      }
+    } catch (error) {
+      core.error(error);
+      return false;
     }
+  } else {
+    core.error('username or password is empty, please check envs' + username);
+    return false;
+  }
+  return true;
 }
+
+let flag = true;
 
 for (let idx = 0; idx < users.length; idx++) {
-    const { username, password } = users[idx]
-    await pause(username, password)
+  const { username, password } = users[idx];
+  const res = await pause(username, password);
+  flag = flag && res;
 }
 
-
+if (!flag) core.setFailed('something went wrong! please check the logs.');
